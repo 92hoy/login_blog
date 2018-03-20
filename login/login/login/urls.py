0c01067e3fe595ae94bf614196df2ef3e2a4d70d
92hoy <ho_9209@naver.com>
2018-03-20 17:25:14 +0900

    로그인 인증 ok

--- login/login/login/urls.py
+++ login/login/login/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^main/', include('main.urls')),
+
+
 ]
--- login/login/main/urls.py
+++ login/login/main/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^regist$', views.regist, name='regist'),
+    url(r'^log_in$', views.log_in, name='log_in'),
+    url(r'^log_out$', views.log_out, name='log_out$'),
+    url(r'^Home', views.Home, name='Home'),
 ]
